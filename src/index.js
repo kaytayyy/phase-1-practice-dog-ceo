329b44d68058422206043b9b6b54c4d7ee87d51e
Inda Smith <inda.k.smith@gmail.com>
2023-03-28 14:07:28 -0400

    added some content, haven't tried it out yet

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 console.log('%c HI', 'color: firebrick')
+//on page load, fetches the images using the url
+document.addEventListener('DOMContentLoaded', ()=>{fetchData})
+function fetchData() {
+    fetch(imgUrl) , {
+        method: "image"
+    }
+}
+
+//parses the response as `JSON`
+.then(res => res.json)
+
+//adds image elements to the DOM **for each** 🤔 image in the array
